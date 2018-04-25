bef54ac21414691b8b8ddb9af818f2b5b1f8d796
moskowitza <moskowitzaaron@mac.com>
2018-04-25 15:59:29 -0400

    sql up

--- webStore.js
+++ webStore.js
@@ -10,3 +10,22 @@
 // 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
 //    * This means updating the SQL database to reflect the remaining quantity.
 //    * Once the update goes through, show the customer the total cost of their purchase.
+
+var inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+      type: "list",
+      name: "product",
+      message: "What are you buying?",
+      choices: ["guitar","bass","drums","tent","sleeping bag","sleeping mat","tofu","seitan","cliff bar","wraps"]  //can we get this to populate?
+    },
+      {
+      name: "amount",
+      message: "How many?",
+    }
+  ]).then(function(user) {
+      if(product(amount))
+  
+  });
+  
